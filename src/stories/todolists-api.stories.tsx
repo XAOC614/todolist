266665e288834ed773bd26e2ee6e4b01f8f9591a
Сергей Фомин <xaoc615@gmail.com>
2022-12-21 23:28:23 +0300

    les 13 API on Stories

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,14 +1,21 @@
 import React, {useEffect, useState} from 'react'
-
+import axios from 'axios'
 export default {
     title: 'API'
 }
-
+const settings = {
+    withCredentials: true,
+    headers:{
+        'API-KEY':'0a18320d-7a98-4203-b236-40819976048c'
+    }
+}
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        // здесь мы будем делать запрос и ответ закидывать в стейт.
-        // который в виде строки будем отображать в div-ке
+       axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',settings)
+           .then((res)=>{
+                setState(res.data)
+            })
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -16,13 +23,21 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const data={title:'Lesson 13'}
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',data,settings)
+            .then(()=>{})
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
+    const todolistId = 'c1ad469e-0f0c-43c7-b0a3-2c4813043294'
     useEffect(() => {
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,settings)
+            .then(()=>{
+
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
